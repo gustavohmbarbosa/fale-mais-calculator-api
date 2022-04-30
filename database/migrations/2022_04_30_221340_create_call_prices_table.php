08dd8218d5938da239a909ad693b2b92c1353841
Gustavo Melo <gustavobarbosa-18@hotmail.com>
2022-04-30 20:34:57 -0300

    feat: add call prices table migration

---
+++ database/migrations/2022_04_30_221340_create_call_prices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('call_prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('origin');
+            $table->unsignedBigInteger('destiny');
+            $table->float('rat_per_minute', 5, 2);
+            $table->timestamps();
+
+            $table->foreign('origin')->references('id')->on('codes');
+            $table->foreign('destiny')->references('id')->on('codes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('call_prices');
+    }
+};
